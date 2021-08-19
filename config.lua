b1d49101877e91cb5f84339b1f92810e5d825f22
vanillaa <80897991+vanillla1@users.noreply.github.com>
2021-08-19 14:51:20 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,7 +2,7 @@ Config = {}
 
 Config.Locations = {
     ["courthouse"] = {
-        enter = vector(234.83, -390.8, -84.94, 178.5),
-        exit = vector(233.01, -411.27, 48.11, 342.5),
+        enter = vector4(234.83, -390.8, -84.94, 178.5),
+        exit = vector4(233.01, -411.27, 48.11, 342.5),
    },
 }
